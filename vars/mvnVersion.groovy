b2e8e586ae2a53290fcad047ddd3b3bd34c5abbd
Victor Cetinic <vcetinick@signpost6.com.au>
2020-12-14 00:02:54 +0100

    Added mvnVersion command

---
+++ vars/mvnVersion.groovy
@@ -0,0 +1,18 @@
+/**
+ * Determines the current maven version of the project
+ *
+ * Usage: mvnVersion()
+ *
+ * @param body
+ */
+
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    File f = new File()
+    println("File - " + f)
+
+}
